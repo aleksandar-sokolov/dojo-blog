4913ccb8db89aaad1b039000b4c9dd8c651ebf7f
Aleksandar <sokolow.aleksandar@gmail.com>
2021-03-25 10:41:22 +0200

    Add home page

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { useState, useEffect } from "react";
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState(
+        [
+            {title: 'sdfsdgfdg', body: 'dsafsdf,', author:'Gogo', id: 1},
+            {title: 'dfgdfgb', body: 'fgdfgdfgbbngfbf,', author:'Todor', id: 2}
+        ]
+    )
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
